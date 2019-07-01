3acde462fd4b7aadbf46a56d92b265e9f380154a
lumengxin <2735146963@qq.com>
2019-07-01 14:24:55 +0800

    nodejs

---
+++ nodelover/Nodejs1/npm-me/a.js
@@ -0,0 +1,11 @@
+/* const index = require('./index.js')
+
+console.log(index);
+
+index(1);
+
+console.log(index.index); */
+
+import p from './index'
+
+console.log(p);
---
+++ nodelover/Nodejs1/npm-me/index.js
@@ -0,0 +1,16 @@
+// CommonJS
+/* module.exports = exports = (x) => {
+    console.log(x);
+} 
+
+exports.index = 2; */
+
+// ES6
+// 需要配置babel
+export var name = "xio";
+export var name2 = "li";
+
+export default {
+    name: "xin",
+    age: "21"
+}
